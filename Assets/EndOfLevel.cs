f0d79386c67d19a48ffee63d1301176ba8d37624
pthauany <spacheco.thauany18@gmail.com>
2021-01-19 10:38:11 -0300

    Interacoes com GoogleVR

---
+++ Assets/EndOfLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EndOfLevel : MonoBehaviour
+{
+
+    public void endOfGame()
+    {
+        //Debug.Log("HIT");
+        //Application.Quit();
+        SceneManager.LoadScene("fps");
+    }
+}
--- Assets/PlayerMoviment.cs
+++ Assets/PlayerMoviment.cs
@@ -36,7 +36,7 @@ public class playerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * y;
+        Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
